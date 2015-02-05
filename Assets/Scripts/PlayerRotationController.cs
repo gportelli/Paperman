4cef3ce9d518b1669b94ddd556cee33652933b92
Giuseppe Portelli <giuseppe.portelli@gmail.com>
2015-02-05 18:18:46 +0100

    Better stall handling

--- Assets/Scripts/PlayerRotationController.cs
+++ Assets/Scripts/PlayerRotationController.cs
@@ -6,15 +6,16 @@ public class PlayerRotationController : MonoBehaviour {
     public float Y_Torque = 0.025f;
     public float Z_Torque = 0.001f;
 
+    // x rotation range (negative up!)
     public float MinXRot = -65f;
     public float MaxXRot = 65f;
 
     private float rotateX = 0, rotateY = 0;
-    private float maxSpeed;
+    private float terminalFrontSpeed;
 
 	void Start () {
         PlayerAerodynamicsController pac = GetComponent<PlayerAerodynamicsController>();
-        maxSpeed = pac.getTerminalFrontSpeed();
+        terminalFrontSpeed = pac.getTerminalFrontSpeed();
 	}
 	
 	void FixedUpdate () {
@@ -37,9 +38,9 @@ public class PlayerRotationController : MonoBehaviour {
         //float ry = Helper.GetRotation(transform.eulerAngles.y);
         float rz = Helper.GetRotation(transform.eulerAngles.z);
 
-        float minXRot = GetMinXRot();
-
         // Adjust x rotation
+        float minXRot = GetMinXRot();
+        
         if (rotateX >= 0 && rx > MaxXRot - epsilon)
         {
             rotateX = (MaxXRot - rx) * X_Torque;
@@ -52,18 +53,25 @@ public class PlayerRotationController : MonoBehaviour {
         rigidbody.AddRelativeTorque(Vector3.right * rotateX * X_Torque);
         rigidbody.AddTorque(Vector3.up * rotateY * Y_Torque);
 
+        
         if (Mathf.Abs(rotateY) > epsilon)
             desiredZRotation = -30 * Mathf.Sign(rotateY);
 
         if (Mathf.Abs(rz - desiredZRotation) > 1)
             rigidbody.AddRelativeTorque(Vector3.forward * -(rz - desiredZRotation) * Z_Torque);
+         
     }
 
+    // stall when front velocity reaches zero
     private float GetMinXRot()
     {
-        float v = Vector3.Dot(gameObject.rigidbody.velocity, transform.forward);
+        float frontVelocity = Vector3.Dot(gameObject.rigidbody.velocity, transform.forward);
+        
+        float k;
+        //k = (frontVelocity - terminalFrontSpeed / 5) * 4 / terminalFrontSpeed;
+        k = frontVelocity / terminalFrontSpeed * 4;
 
-        return Mathf.Lerp(0, MinXRot, (v - maxSpeed / 5) * 4 / maxSpeed);
+        return Mathf.Lerp(0, MinXRot, k);
     }
 
 }
