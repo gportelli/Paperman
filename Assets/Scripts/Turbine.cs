44f45e96debae150aabfe4f228ed33d1c0995c36
Giuseppe Portelli <giuseppe.portelli@gmail.com>
2015-02-07 22:53:47 +0100

    Turbine element added

---
+++ Assets/Scripts/Turbine.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turbine : MonoBehaviour {
+    public float RotationSpeed = 200;
+    public float BoostMagnitude = 1;
+    public float BoostDuration = 1;
+
+    public Transform turbineA, turbineB;
+
+    private TurbineBoost turbineBoost;
+
+    void Awake()
+    {
+        turbineBoost = GameObject.FindGameObjectWithTag("Player").GetComponent<TurbineBoost>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "PlayerCollider")
+        {
+            turbineBoost.SetBoost(transform.forward * BoostMagnitude, BoostDuration);
+        }
+    }
+
+	void Update () {
+        turbineA.Rotate(0, 0, Time.deltaTime * RotationSpeed);
+        turbineB.Rotate(0, 0, -Time.deltaTime * RotationSpeed);
+	}
+}
---
+++ Assets/Scripts/TurbineBoost.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurbineBoost : MonoBehaviour {
+    private Vector3 currBoost;
+    private float duration;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (duration > 0) duration -= Time.deltaTime;
+        if (duration < 0) duration = 0;
+	}
+
+    public void SetBoost(Vector3 boostForce, float duration)
+    {
+        currBoost = boostForce;
+        this.duration = duration;
+    }
+
+    public Vector3 GetBoost()
+    {
+        if (duration > 0)
+            return currBoost;
+        else
+            return Vector3.zero;
+    }
+}
